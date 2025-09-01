c2a7c716aaa928504c93f5175de8a6073b75ed84
faustoche <faustine.crocq@gmail.com>
2025-09-01 14:07:34 +0200

    clients clean + handle clients

--- srcs/clients/clients.hpp
+++ srcs/clients/clients.hpp
@@ -1,51 +1,51 @@
-// #pragma once
+#pragma once
 
-// /************ INCLUDES ************/
+/************ INCLUDES ************/
 
-// #include <poll.h>
-// #include "server.hpp"
-// #include <sys/time.h>
+#include <poll.h>
+#include "server.hpp"
+#include <sys/time.h>
 
-// /************ DEFINE ************/
+/************ DEFINE ************/
 
-// using namespace std;
+using namespace std;
 
-// enum client_state {
-// 	READING,
-// 	PROCESSING,
-// 	SENDING,
-// 	DISCONNECTED
-// };
+enum client_state {
+	READING,
+	PROCESSING,
+	SENDING,
+	DISCONNECTED
+};
 
-// /************ CLASS ************/
+/************ CLASS ************/
 
-// /* On conserve l'heure de connection du derneir client pour pouvoir définir un timeout ou identifier une inactivité*/
+/* On conserve l'heure de connection du derneir client pour pouvoir définir un timeout ou identifier une inactivité*/
 
-// class c_client
-// {
-// private:
-// 	int				_fd;
-// 	client_state	_state;
-// 	char			_buffer[4096];
-// 	struct timeval	_timestamp;
-// 	string			_read_buffer;
-// 	string			_write_buffer;
-// 	size_t			_bytes_written;
+class c_client
+{
+private:
+	int				_fd;
+	client_state	_state;
+	char			_buffer[4096];
+	struct timeval	_timestamp;
+	string			_read_buffer;
+	string			_write_buffer;
+	size_t			_bytes_written;
 
-// public:
-// 	c_client();
-// 	c_client(int client_fd);
-// 	~c_client();
+public:
+	c_client();
+	c_client(int client_fd);
+	~c_client();
 
-// 	/******* GETTERS ******* */
-// 	const int &get_fd() const { return (_fd); }
-// 	const struct timeval &get_timestamp() const { return (_timestamp); }
-// 	const string &get_read_buffer() const { return (_read_buffer); }
-// 	const string &get_write_buffer() const { return (_write_buffer); }
-// 	const size_t &get_bytes_written() const { return (_bytes_written); }
-// 	const client_state &get_state() const { return (_state); }
+	/******* GETTERS ******* */
+	const int &get_fd() const { return (_fd); }
+	const struct timeval &get_timestamp() const { return (_timestamp); }
+	const string &get_read_buffer() const { return (_read_buffer); }
+	const string &get_write_buffer() const { return (_write_buffer); }
+	const size_t &get_bytes_written() const { return (_bytes_written); }
+	const client_state &get_state() const { return (_state); }
 
-// 	/******* SETTERS ******* */
-// 	//void set_state(client_state new_state) { (_state == new_state); }
-// 	//void set_bytes_written(size_t bytes) { (_bytes_written == bytes); }
-// };
+	/******* SETTERS ******* */
+	//void set_state(client_state new_state) { (_state == new_state); }
+	//void set_bytes_written(size_t bytes) { (_bytes_written == bytes); }
+};
