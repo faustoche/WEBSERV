803fc4ceaa2b3f9f5394774dae75088a98936fdb
faustoche <faustine.crocq@gmail.com>
2025-09-29 12:19:56 +0200

    trying the merge

--- srcs/server_config/parser.cpp
+++ srcs/server_config/parser.cpp
@@ -186,7 +186,7 @@ void                c_parser::location_url_file(c_server & server)
 /*------------------------   location : directives   ---------------------------*/
 void                c_parser::location_directives(c_location & location)
 {
-   // int     flag_cgi = 0;
+    int     flag_cgi = 0;
     int     flag_upload = 0;
 
     if (is_token_value("cgi"))
